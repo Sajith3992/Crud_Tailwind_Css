abe8ba77908fb2a558afb763f2f371daf50fc3f8
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:34:24 +0530

    push the product item

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const [product, setProduct] = useState({
 
   const handleSubmit = (e) =>{
     e.priventDetault();
+    item.push(product);
   }
   return (
     <div className="grid grid-cols-2 mt-56">
