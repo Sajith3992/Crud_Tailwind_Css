e66b1ecd3be6270909b4906fd36c186ecff7c670
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 09:18:55 +0530

    button class name value change

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const [product, setProduct] = useState({
           className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product Price'/>
           <br/>
-          <button className=''>Submit</button>
+          <button className='px-10 py-2 text-md text-white bg-gray-600 rounded '>Submit</button>
       </form>
       </div> 
       </div>
