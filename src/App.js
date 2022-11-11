b0a7a1044cd66675a1cd831c69c570d561b2b02a
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:26:53 +0530

    add the input tag to name value and onchange

--- src/App.js
+++ src/App.js
@@ -21,19 +21,35 @@ const [product, setProduct] = useState({
       <h1 className='uppercase text-2xl text-center'>add product</h1>
       <form>
           <label className='block'>ID  </label>
-          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          <input
+                 name='id'
+                 value={product.id}
+                 onChange={handleChange}
+          className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product id'/>
             <br/>
           <label className='block'>Name  </label>
-          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          <input
+                name='name'
+                value={product.name}
+                onChange={handleChange}
+           className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product Name'/>
             <br/>
           <label className='block'>Quantity  </label>
-          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          <input 
+                 name='qty'
+                 value={product.qty}
+                 onChange={handleChange}
+          className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product Qty'/>
             <br/>
           <label className='block'>Price  </label>
-          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          <input
+                 name='price'
+                 value={product.price}
+                 onChange={handleChange}
+          className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product Price'/>
       </form>
       </div> 
