b5099004109d84e6cd2f21661d435e7bc4b18a92
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 05:45:42 +0530

    add the use state to values

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   
-const [product, setProduct] = useState;
+const [product, setProduct] = useState({
+  id: "",
+  name:"",
+  qty:"",
+  price:""
+
+});
 
   return (
     <div>
