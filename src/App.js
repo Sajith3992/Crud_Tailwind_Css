649b908ebfb9cc3b5d5225519105ecc5dd383464
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:28:47 +0530

    change valie to value

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const [product, setProduct] = useState({
 });
 
   const handleChange = (e) =>{
-    setProduct({...product,[e.target.name]: e.target.valie});
+    setProduct({...product,[e.target.name]: e.target.value});
   }
   return (
     <div className="grid grid-cols-2 mt-56">
