98d16d6341897017d5c261d751e315843da82b44
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:06:34 +0530

    get dawn

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ const [product, setProduct] = useState({
       <h1 className='uppercase text-2xl text-center'>add product</h1>
       <form>
           <label className='block'>ID  </label>
-          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' placeholder='input product id'/>
+          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          placeholder='input product id'/>
       </form>
       </div> 
       </div>
