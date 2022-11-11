64c318d3dde0429b18e25ee901aaca0137cd5797
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:11:14 +0530

    add name,qty,price label and input tags and values

--- src/App.js
+++ src/App.js
@@ -19,6 +19,18 @@ const [product, setProduct] = useState({
           <label className='block'>ID  </label>
           <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
           placeholder='input product id'/>
+            <br/>
+          <label className='block'>Name  </label>
+          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          placeholder='input product Name'/>
+            <br/>
+          <label className='block'>Quantity  </label>
+          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          placeholder='input product Qty'/>
+            <br/>
+          <label className='block'>Price  </label>
+          <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' 
+          placeholder='input product Price'/>
       </form>
       </div> 
       </div>
