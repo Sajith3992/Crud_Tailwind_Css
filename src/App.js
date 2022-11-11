27d0f750607dae7e25dfeef4c9d26c5b80e6e933
Sajith3992 <k3992.sajith@gmail.com>
2022-11-11 08:05:44 +0530

    add class name to text center

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const [product, setProduct] = useState({
     <div className="grid grid-cols-2 mt-56">
     <div className='flex justify-center item-center'>
       <div className='bg-white  w-2/5 p-6 rounded'>
-      <h1 className='uppercase text-2xl'>add product</h1>
+      <h1 className='uppercase text-2xl text-center'>add product</h1>
       <form>
           <label className='block'>ID  </label>
           <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' placeholder='input product id'/>
